715641d30092b830c5db5a510191e8c17544e4f5
vrindak <vrindak@ndimensionz.com>
2020-02-26 14:51:21 +0530

    sample changed

--- app/src/main/java/com/ndz/displayapplication/SampleActivity.java
+++ app/src/main/java/com/ndz/displayapplication/SampleActivity.java
@@ -1,22 +1,18 @@
 package com.ndz.displayapplication;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import com.ndz.displayhelper.dialog.DialogManager;
-import com.ndz.displayhelper.dialog.MaterialDialog;
 import com.ndz.displayhelper.dialog.MaterialDialogManager;
-import com.ndz.displayhelper.progress.ProgressManager;
 import com.ndz.displayhelper.toast.ToastManager;
 
 
@@ -29,18 +25,6 @@ public class SampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-      /*  mLayout=findViewById(R.id.layout_login);
-        mLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-               *//* ProgressManager mManager = new ProgressManager.Builder().setmContext(this).setMaxLimit(100).setmMessage("Loading....").setmTitle("Api Call").setStyle(1).setmTheme(0).build();
-                mManager.showProgress();
-               *//*
-            }
-        });*/
 
     }
 
